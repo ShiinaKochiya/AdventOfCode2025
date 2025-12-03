3ff4108e0b94f1adf125e3dc82821ab06b7efea4
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-03 18:08:48 +0700

    the base for day 3

--- day3/day3_part1.java
+++ day3/day3_part1.java
@@ -5,11 +5,22 @@ public class day1 {
     public static void main(String[] args) {
         String FILE_PATH = "./day3/day3_input.txt";
         File file = new File(FILE_PATH);
-
+        int totalVoltage = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
-            while ((line = br.readLine()) != null) {}
-
+            while ((line = br.readLine()) != null) {
+                int max = 0;
+                for(int i = 0; i < line.length() - 1; i++){
+                    for(int j = i + 1; j < line.length(); j++){
+                        String num = line.charAt(i) + "" + line.charAt(j);
+                        int voltage = Integer.parseInt(num);
+                        if (max<voltage) {max = voltage;}
+                    }
+                }
+                totalVoltage += max;
+                System.out.println(max);
+            }
         } catch (IOException e) {}
+        System.out.println(totalVoltage);
     }
 }
