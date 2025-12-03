0526bda43f164ec31a819272ed0e6ced08227974
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-03 15:52:27 +0700

    got it, day 1

--- day1_part2.java
+++ day1_part2.java
@@ -12,16 +12,15 @@ public class day1_part2 {
             while ((line = br.readLine()) != null) {
                 if(line.startsWith("L")){
                     //go back
-                    int oldPointer = pointer;
                     line = line.replaceAll("L", "");
                     int loop = Integer.parseInt(line);
                     while(loop!=0){
                         pointer--;
                         loop--;
+                        if (pointer == -1){pointer = 99;}
                         if (pointer == 0) {
                             pass++;
                         }
-                        if (pointer == -1){pointer = 99;}
                     }
                 } else {
                     //go forward
@@ -30,10 +29,10 @@ public class day1_part2 {
                     while (loop != 0){
                         pointer++;
                         loop--;
+                        if (pointer == 100){pointer = 0;}
                         if (pointer == 0) {
                             pass++;
                         }
-                        if (pointer == 100){pointer = 0;}
                     }
                 }
             }
