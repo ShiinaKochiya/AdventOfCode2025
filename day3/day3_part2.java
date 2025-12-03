5de66b4e1a63b6aef27f223940919015fcb8d1a0
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-03 18:14:18 +0700

    forgot some stuff

--- day3/day3_part2.java
+++ day3/day3_part2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class day3_part1 {
+public class day3_part2 {
     public static void main(String[] args) {
         String FILE_PATH = "./day3/day3_input.txt";
         File file = new File(FILE_PATH);
