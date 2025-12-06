e5e8afc931f353d0b3e768ed78318d1a23bb379b
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-06 15:00:12 +0700

    Day 6 part 2 complete! i love rawdogging lmao

--- day6/day6_part2.js
+++ day6/day6_part2.js
@@ -10,11 +10,11 @@ numList = []
 operationList = []
 
 rl.on('line', (line) => {
-    temp = line.split(" ")
+    temp = line.split("")
     //temp = temp.filter(i => i)
 
     if(isNaN(temp[0])) {
-        operationList = temp
+        operationList = line.split(" ").filter(i => i)
     } else {
         numList.push(temp)
     }
@@ -22,63 +22,60 @@ rl.on('line', (line) => {
 
 total = 0
 maxDigi = []
+
 rl.on('close', () => {
 
-    console.log(numList)
+
+    max = 0
+    for (let i = 0; i < numList.length; i++) {
+        if(numList[i].length > max) max = numList[i].length
+    }
     for(let i = 0; i < numList.length; i++) {
-        for(let j = 0; j < numList[i].length; j++) {
-            if(numList[i][j] === "") {console.log("empty string")} else {console.log(numList[i][j])}
-        }
+        while(numList[i].length < max + 1) numList[i].push(" ")
     }
-/*
-    //checking what is the max digit for each col
-    for(let i = 0; i < numList[0].length; i++) {
-        maxLength = 0
-        for(let j = 0; j < numList.length; j++) {
-            if(numList[j][i].length > maxLength) maxLength = numList[j][i].length
-        }
-        maxDigi.push(maxLength)
-    }
-    //note to self: i ->; j go down
-    //standardizing by adding 0s to make strings length the same
-    for(let i = 0; i < numList[0].length; i++) {
-        for(let j = 0; j < numList.length; j++) {
-            numList[j][i] = numList[j][i].padEnd(maxDigi[i], "0")
-        }
-    }
-    //
+    console.log(numList)
 
-    //copy and rewrite the numbers
-
-    tempList = []
+    numArr = []
+    tempArr = []
     for(let i = 0; i < numList[0].length; i++) {
-        tempArr = []
-        for(let x = numList[0][i].length - 1; x >= 0; x--) {
+        newCol = true
         tempString = ""
-            for(let j = 0; j < numList.length; j++) {
-                tempString += numList[j][i].charAt(x)
+
+        for(let j =  0; j < numList.length; j++) {
+            if(numList[j][i] === " ") {
+                tempString += ""
+            } else {
+                tempString += numList[j][i]
+                newCol = false
             }
-            tempArr.push(tempString)
         }
-        tempList.push(tempArr)
+
+        if(!newCol) {
+            tempArr.push(tempString)
+        } else {
+            numArr.push(tempArr)
+            tempArr = []
+        }
+
     }
-*/
+    numList = numArr
+    operations = operationList.filter(i => i !== " ")
     //console.log(tempList)
     //recycling this part later
-    /*for(let i = 0; i < numList[0].length; i++) {
-        tempTotal = parseInt(numList[0][i])
-        for(let j = 1; j < numList.length; j++) {
+    for(let i = 0; i < numList.length; i++) {
+        tempTotal = parseInt(numList[i][0])
+        for(let j = 1; j < numList[i].length; j++) {
             if(operationList[i] === "+") {
-                console.log("doing " + tempTotal + " + " + parseInt(numList[j][i]))
-                tempTotal += parseInt(numList[j][i])
+                console.log("doing " + tempTotal + " + " + parseInt(numList[i][j]))
+                tempTotal += parseInt(numList[i][j])
             } else {
-                console.log("doing " + tempTotal + " * " + parseInt(numList[j][i]))
-                tempTotal = tempTotal * parseInt(numList[j][i])
+                console.log("doing " + tempTotal + " * " + parseInt(numList[i][j]))
+                tempTotal = tempTotal * parseInt(numList[i][j])
             }
         }
         total += tempTotal
         console.log(tempTotal)
     }
-    console.log(total)*/
+    console.log(total)
 
 })
--- day6/temp.js
+++ day6/temp.js
@@ -14,13 +14,48 @@ rl.on('line', (line) => {
     //temp = temp.filter(i => i)
 
     if(isNaN(temp[0])) {
-        operationList = temp
+        operationList = line.split(" ").filter(i => i)
     } else {
         numList.push(temp)
     }
 });
 
 rl.on('close', () => {
+
+    max = 0
+    for (let i = 0; i < numList.length; i++) {
+        if(numList[i].length > max) max = numList[i].length
+    }
+    for(let i = 0; i < numList.length; i++) {
+        while(numList[i].length < max + 1) numList[i].push(" ")
+    }
     console.log(numList)
+
+    numArr = []
+    tempArr = []
+    for(let i = 0; i < numList[0].length; i++) {
+        newCol = true
+        tempString = ""
+
+        for(let j =  0; j < numList.length; j++) {
+            console.log("reading" + numList[j][i])
+            if(numList[j][i] === " ") {
+                tempString += ""
+            } else {
+                tempString += numList[j][i]
+                newCol = false
+            }
+        }
+
+        if(!newCol) {
+            tempArr.push(tempString)
+        } else {
+            numArr.push(tempArr)
+            tempArr = []
+        }
+
+    }
+    operations = operationList.filter(i => i != " ")
+    console.log(numArr)
     console.log(operationList)
 })
