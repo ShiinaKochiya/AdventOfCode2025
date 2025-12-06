136be1d982189984ae37f89d4830b23d26e7c519
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-06 12:09:32 +0700

    day 6 init

---
+++ day6/day6_part1.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('day6_input.txt'),
+    crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+    line
+});
+
+    totalValid = 0
+
+rl.on('close', () => {
+
+})
