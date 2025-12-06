502afdf3f223e5aa49bfdcd4a0d8f2ecf0b780d9
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-06 12:30:19 +0700

    Day 6 part 1 done

--- day6/day6_part1.js
+++ day6/day6_part1.js
@@ -6,12 +6,38 @@ const rl = readline.createInterface({
     crlfDelay: Infinity
 });
 
+numList = []
+operationList = []
+
 rl.on('line', (line) => {
-    line
+    temp = line.split(" ")
+    temp = temp.filter(i => i)
+
+    if(isNaN(temp[0])) {
+        operationList = temp
+    } else {
+        numList.push(temp)
+    }
 });
 
-    totalValid = 0
+total = 0
 
 rl.on('close', () => {
-
+    console.log(numList)
+    console.log(operationList)
+    for(let i = 0; i < numList[0].length; i++) {
+        tempTotal = parseInt(numList[0][i])
+        for(let j = 1; j < numList.length; j++) {
+            if(operationList[i] === "+") {
+                console.log("doing " + tempTotal + " + " + parseInt(numList[j][i]))
+                tempTotal += parseInt(numList[j][i])
+            } else {
+                console.log("doing " + tempTotal + " * " + parseInt(numList[j][i]))
+                tempTotal = tempTotal * parseInt(numList[j][i])
+            }
+        }
+        total += tempTotal
+        console.log(tempTotal)
+    }
+    console.log(total)
 })
