97ffd0b3b9d943c90ed785f7583a9b67e117c7ac
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-08 15:06:51 +0700

    Day 7 part 1 done
    im not doing part 2 cos it looks really hard lmao

--- day7/day7_part1.js
+++ day7/day7_part1.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const readline = require('readline');
 
@@ -7,12 +6,42 @@ const rl = readline.createInterface({
     crlfDelay: Infinity
 });
 
+matrix = []
+
 rl.on('line', (line) => {
     temp = line.split("")
-
+    matrix.push(temp)
 });
 
 
 rl.on('close', () => {
+    loc = []
+    branc = 0
+    loc.push(matrix[0].indexOf("S"))
+    for(let i = 1; i < matrix.length; i++) {
+        //locate where agnes tachyon would be
+        arcPop = []
 
+        for(let j = 0; j < loc.length; j++) {
+            if(matrix[i][loc[j]] === "^") {
+                arcPop.push(loc[j])
+                console.log("tachyon at " + loc[j])
+                branc++
+            }
+        }
+        loc = loc.filter(i => !arcPop.includes(i))
+
+        for(let j = 0; j < arcPop.length; j++) {
+            if(arcPop[j] - 1 > 0) loc.push(arcPop[j] - 1)
+
+        }
+        for(let j = 0; j < arcPop.length; j++) {
+            if(arcPop[j] + 1 < matrix[i].length) loc.push(arcPop[j] + 1)
+        }
+        loc = loc.filter((item, pos) => loc.indexOf(item) == pos)
+        console.log("ARC Pops location:" + arcPop)
+        console.log("Locations of stream:" + loc)
+    }
+    console.log(loc.length)
+    console.log(branc + "<---")
 })
