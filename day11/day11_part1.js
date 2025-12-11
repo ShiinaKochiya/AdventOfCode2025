c8d9868d08e6148b3c1c274a55b0140cc4a78c1d
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-12-11 17:36:03 +0700

    Tôi yêu đệ quy

---
+++ day11/day11_part1.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const readline = require('readline');
+const startTime = performance.now();
+const rl = readline.createInterface({
+    input: fs.createReadStream('day11_input.txt'),
+    crlfDelay: Infinity
+});
+
+serverMap = []
+
+rl.on('line', (line) => {
+    rack = line.split(" ")
+    serverMap.push(rack)
+});
+
+rl.on('close', () => {
+    let endReached = 0;
+    const graph = {};
+
+    for (const arr of serverMap) {
+        const key = arr[0];
+        graph[key] = arr.slice(1);
+    }
+
+    function dfs(node) {
+        for (const next of graph[node]) {
+            if (next === "out") {
+                endReached++;
+            } else {
+                dfs(next + ":");
+            }
+        }
+    }
+
+    const start = serverMap.find(x => x[0] === "you:");
+
+    for (let i = 1; i < start.length; i++) {
+        dfs(start[i] + ":");
+    }
+
+    console.log(endReached);
+});
